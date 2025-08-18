3d766878c805ebffe60e808e4447780f0162624e
madaisofia <madaisofiajimenez@gmail.com>
2025-08-17 22:17:04 -0600

    Create TopicoRepository.java

---
+++ src/main/java/com/forohub/repository/TopicoRepository.java
@@ -0,0 +1,9 @@
+package com.forohub.repository;
+
+import com.forohub.model.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    Optional<Topico> findByTituloAndMensaje(String titulo, String mensaje);
+}
